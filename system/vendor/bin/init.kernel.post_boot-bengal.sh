21fe7b6cceb82bf1f59180866bf69f4f71ee069b
John Vincent <git@tenseventyseven.cf>
2023-07-22 20:59:09 +0800

    vendor: Make input boost longer
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- system/vendor/bin/init.kernel.post_boot-bengal.sh
+++ system/vendor/bin/init.kernel.post_boot-bengal.sh
@@ -319,7 +319,7 @@ echo 1 > /proc/sys/walt/sched_min_task_util_for_colocation
 # configure input boost settings
 echo 1190000 0 0 0 0 0 0 0 > /proc/sys/walt/input_boost/input_boost_freq
 echo 2 > /proc/sys/walt/input_boost/sched_boost_on_input # migrate tasks to big on input
-echo 150 > /proc/sys/walt/input_boost/input_boost_ms
+echo 250 > /proc/sys/walt/input_boost/input_boost_ms
 
 # configure powerkey boost settings
 echo 1804800 0 0 0 2208000 0 0 0 > /proc/sys/walt/input_boost/powerkey_input_boost_freq
