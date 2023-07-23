daa19c20865f35dbeb88995c8d4dd969a3ffec55
John Vincent <git@tenseventyseven.cf>
2023-07-23 16:39:23 +0800

    vendor: Re-strategize migration margins
    Let's not trigger load balancing too frequently and fit more tasks in a Silver core. WALT uses the upmigrate margin as the load balancing indicator as well (for some reason) so we really want to set this to a high, yet appropriate value.
    
    We shouldn't worry about foreground and top-app tasks since those are boosted on touch and everytime, respectively. They're going to be on Gold CPUs often without even checking for margins.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- system/vendor/bin/init.kernel.post_boot-bengal.sh
+++ system/vendor/bin/init.kernel.post_boot-bengal.sh
@@ -227,15 +227,15 @@ echo 100 > /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms
 echo 4 > /sys/devices/system/cpu/cpu4/core_ctl/task_thres
 
 # Set up optimized scheduler parameters for khaje-2 (SDM685)
-echo 47 > /proc/sys/walt/sched_downmigrate
-echo 60 > /proc/sys/walt/sched_upmigrate
-echo 85 > /proc/sys/walt/sched_group_downmigrate
-echo 95 > /proc/sys/walt/sched_group_upmigrate
+echo 55 > /proc/sys/walt/sched_downmigrate
+echo 80 > /proc/sys/walt/sched_upmigrate
+echo 75 > /proc/sys/walt/sched_group_downmigrate
+echo 90 > /proc/sys/walt/sched_group_upmigrate
 echo 1 > /proc/sys/walt/sched_walt_rotate_big_tasks
 
 # Set up early migrate tunables for tasks in RTG
 sched_early_downmigrate=40
-sched_early_upmigrate=50
+sched_early_upmigrate=60
 echo "$((1024 * 100 / $sched_early_downmigrate))" > /proc/sys/walt/sched_early_downmigrate
 echo "$((1024 * 100 / $sched_early_upmigrate))" > /proc/sys/walt/sched_early_upmigrate
 
