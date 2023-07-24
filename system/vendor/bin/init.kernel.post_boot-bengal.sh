0ef4dc03e637e795c33bc976a929087e66180e37
John Vincent <git@tenseventyseven.cf>
2023-07-24 20:23:09 +0800

    Revert "vendor: Test using migration margins from the 660"
    This reverts commit 3a217924375424fc75b2a0dfc3125c41b135de5b.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- system/vendor/bin/init.kernel.post_boot-bengal.sh
+++ system/vendor/bin/init.kernel.post_boot-bengal.sh
@@ -227,8 +227,8 @@ echo 100 > /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms
 echo 4 > /sys/devices/system/cpu/cpu4/core_ctl/task_thres
 
 # Set up optimized scheduler parameters for khaje-2 (SDM685)
-echo 67 > /proc/sys/walt/sched_downmigrate
-echo 77 > /proc/sys/walt/sched_upmigrate
+echo 60 > /proc/sys/walt/sched_downmigrate
+echo 80 > /proc/sys/walt/sched_upmigrate
 echo 85 > /proc/sys/walt/sched_group_downmigrate
 echo 100 > /proc/sys/walt/sched_group_upmigrate
 echo 1 > /proc/sys/walt/sched_walt_rotate_big_tasks
