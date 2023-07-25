7c82c76257dd62c0707368ca648408b1297fde75
John Vincent <git@tenseventyseven.cf>
2023-07-25 21:15:41 +0800

    Revert "vendor: Require idle CPUs for foreground tasks"
    There's just too many of them. This reverts commit 17986ce43e04cc5edad61740662ac4f3b7d9811e.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- system/vendor/bin/init.kernel.post_boot-bengal.sh
+++ system/vendor/bin/init.kernel.post_boot-bengal.sh
@@ -270,7 +270,6 @@ echo 0-3 > /dev/cpuset/restricted/cpus
 # uclamp parameters
 echo 1 > /dev/cpuctl/camera-daemon/cpu.uclamp.latency_sensitive
 echo 1 > /dev/cpuctl/top-app/cpu.uclamp.latency_sensitive
-echo 1 > /dev/cpuctl/foreground/cpu.uclamp.latency_sensitive
 echo 1 > /dev/cpuctl/camera-daemon/cpu.uclamp.min
 echo 1 > /dev/cpuctl/top-app/cpu.uclamp.min
 
