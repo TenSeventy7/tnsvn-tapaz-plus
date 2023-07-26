d043142320b07ff55848f78cb49f79fe24475e39
John Vincent <git@tenseventyseven.cf>
2023-07-26 20:24:50 +0800

    vendor: Allow RTGs to upmigrate earlier and downmigrate later
    RTGs are only enabled for 'top-app' tasks and we want these to be on Gold CPUs most of the time to improve performance. Have them migrate up if a group has a demand of at least 18ms (90% of 20ms), and only downmigrate these groups if they only demand 15ms (75% of 20ms).
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- system/vendor/bin/init.kernel.post_boot-bengal.sh
+++ system/vendor/bin/init.kernel.post_boot-bengal.sh
@@ -229,8 +229,8 @@ echo 4 > /sys/devices/system/cpu/cpu4/core_ctl/task_thres
 # Set up optimized scheduler parameters for khaje-2 (SDM685)
 echo 60 > /proc/sys/walt/sched_downmigrate
 echo 80 > /proc/sys/walt/sched_upmigrate
-echo 85 > /proc/sys/walt/sched_group_downmigrate
-echo 100 > /proc/sys/walt/sched_group_upmigrate
+echo 75 > /proc/sys/walt/sched_group_downmigrate
+echo 90 > /proc/sys/walt/sched_group_upmigrate
 echo 1 > /proc/sys/walt/sched_walt_rotate_big_tasks
 echo 10 > /proc/sys/walt/sched_ed_boost
 
