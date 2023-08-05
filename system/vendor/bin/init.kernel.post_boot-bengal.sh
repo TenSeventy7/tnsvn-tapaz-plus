3156639bda14110534a9342923b43bba25c6f567
John Vincent <git@tenseventyseven.cf>
2023-08-05 15:48:21 +0800

    vendor: Re-adjust group task migrate thresholds
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- system/vendor/bin/init.kernel.post_boot-bengal.sh
+++ system/vendor/bin/init.kernel.post_boot-bengal.sh
@@ -229,14 +229,14 @@ echo 4 > /sys/devices/system/cpu/cpu4/core_ctl/task_thres
 # Set up optimized scheduler parameters for khaje-2 (SDM685)
 echo 40 > /proc/sys/walt/sched_downmigrate
 echo 60 > /proc/sys/walt/sched_upmigrate
-echo 10 > /proc/sys/walt/sched_group_downmigrate
-echo 10 > /proc/sys/walt/sched_group_upmigrate
+echo 80 > /proc/sys/walt/sched_group_downmigrate
+echo 95 > /proc/sys/walt/sched_group_upmigrate
 echo 0 > /proc/sys/walt/sched_walt_rotate_big_tasks
 echo 5 > /proc/sys/walt/sched_ed_boost
 
 # Set up early migrate tunables for tasks in RTG
-sched_early_downmigrate=10
-sched_early_upmigrate=10
+sched_early_downmigrate=40
+sched_early_upmigrate=60
 echo "$((1024 * 100 / $sched_early_downmigrate))" > /proc/sys/walt/sched_early_downmigrate
 echo "$((1024 * 100 / $sched_early_upmigrate))" > /proc/sys/walt/sched_early_upmigrate
 
