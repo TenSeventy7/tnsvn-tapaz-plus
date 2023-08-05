13f922e639530c41c2a0e501369805a3402563d2
John Vincent <git@tenseventyseven.cf>
2023-08-05 15:59:53 +0800

    vendor: Set min util for uclamp boosting to '1'
    '0' doesn't seem to work properly.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- system/vendor/bin/init.kernel.post_boot-bengal.sh
+++ system/vendor/bin/init.kernel.post_boot-bengal.sh
@@ -300,7 +300,7 @@ echo 0 > /sys/devices/system/cpu/cpufreq/policy4/walt/rtg_boost_freq
 
 # colocation V3 settings
 echo 940800 > /sys/devices/system/cpu/cpufreq/policy0/walt/rtg_boost_freq
-echo 0 > /proc/sys/walt/sched_min_task_util_for_uclamp
+echo 1 > /proc/sys/walt/sched_min_task_util_for_uclamp
 echo 0 > /proc/sys/walt/sched_min_task_util_for_colocation
 
 # configure input boost settings
